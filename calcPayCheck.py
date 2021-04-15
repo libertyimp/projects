983df92d70b22ac47837553824a5f10def42510f
KoreyMoffett <korey.moffett87@gmail.com>
2021-04-14 23:26:12 -0500

    added pay check calc

---
+++ calcPayCheck.py
@@ -0,0 +1,7 @@
+# This program is written in Python and it can help you calculate your gross pay.
+
+hours = input('Enter the number of hours the employee works: ')
+payRate = input('Enter the employee\'s hourly pay rate: ')
+grossPay = hours * payRate
+
+print("The employee\'s gross pay is $" + str(grossPay))
