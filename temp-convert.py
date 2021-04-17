86df32667e350104e22ac308774ba06f6773a125
KoreyMoffett <korey.moffett87@gmail.com>
2021-04-17 00:25:48 -0500

    Temp converter written in python

---
+++ temp-convert.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+
+choice = input("Will you be converting\n1.) C - F\nor\n2.) F - C\n >> ")
+
+# Allow for choice of F-C or C-F
+if choice == 1:
+   metric_temp = (float(input("Enter the temp in celsius: ")))
+   imper_temp = (1.8 * metric_temp) + 32
+   print ('Fahrenheit:', imper_temp)
+
+elif choice == 2:
+    imper_temp = float(input("Enter the temp in fahrenheit: "))
+    metric_temp = (imper_temp - 32) / 1.8
+    print ('Celsius:', metric_temp)
